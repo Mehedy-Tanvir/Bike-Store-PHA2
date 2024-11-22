9a8e0329f333c1e4bf2e22715cea00114c429a8a
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-11-22 10:15:33 +0600

    project initialized

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,154 @@
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
+import {
+  TGuardian,
+  TLocalGuardian,
+  TStudent,
+  StudentMethods,
+  StudentModel,
+  TUserName,
+} from './student.interface';
+
+import bcrypt from 'bcrypt';
+import config from '../../config/index';
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: [true, 'first name is required'],
+    trim: true,
+    maxlength: [20, 'first name can not more than 20 character'],
+    validate: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: 'first name first letter should be capital',
+    },
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'last name is required'],
+    validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: `{VALUE} is not valid`,
+    },
+  },
+});
+
+const localGuardianSchema = new Schema<TLocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<TGuardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+
+const studentSchema = new Schema<TStudent, StudentModel>(
+  {
+    id: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    name: {
+      type: userNameSchema,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ['male', 'female', 'other'],
+        message: `{VALUE} is not valid. The gender can be 'male', 'female', 'other'`,
+      },
+      required: true,
+    },
+    dateOfBirth: { type: String },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (value: string) => validator.isEmail(value),
+        message: `{VALUE} is not valid email type`,
+      },
+    },
+    contactNumber: { type: String, required: true },
+    emergencyContactNumber: { type: String, required: true },
+    bloodGroup: {
+      type: String,
+      enum: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'],
+    },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+    guardian: {
+      type: guardianSchema,
+      required: true,
+    },
+    localGuardian: {
+      type: localGuardianSchema,
+      required: true,
+    },
+    profileImage: { type: String },
+    isActive: {
+      type: String,
+      enum: ['active', 'blocked'],
+      default: 'active',
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+// virtual
+studentSchema.virtual('fullName').get(function () {
+  return `${this.name.firstName} ${this.name.middleName} ${this.name.lastName}`;
+});
+
+// pre save middleware or hook : will work on create() save()
+studentSchema.pre('save', async function () {
+  // console.log(this, 'pre hook: we will save the data');
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  // hashing password and save into db
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+});
+
+// post save middleware or hook
+studentSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+  // console.log(this, 'post hook: we saved the data');
+});
+
+// query middleware
+studentSchema.pre('find', function (next) {
+  console.log(this);
+});
+
+// creating a custom static method
+studentSchema.statics.isUserExists = async function (id: string) {
+  const existingUser = await Student.findOne({ id });
+  return existingUser;
+};
+
+// creating a custom instance method
+// studentSchema.methods.isUserExists = async function (id: string) {
+//   const existingUser = await Student.findOne({ id });
+//   return existingUser;
+// };
+
+export const Student = model<TStudent, StudentModel>('Student', studentSchema);
---
+++ src/app/modules/student/student.service.ts
@@ -0,0 +1,32 @@
+import { TStudent } from './student.interface';
+import { Student } from './student.model';
+
+const createStudentIntoDB = async (student: TStudent) => {
+  if (await Student.isUserExists(student.id)) {
+    throw new Error('User already exists');
+  }
+  const result = await Student.create(student);
+  // const newStudent = new Student(student);
+  // if (await newStudent.isUserExists(student.id)) {
+  //   throw new Error('Student already exists');
+  // }
+  // const result = await newStudent.save();
+
+  return result;
+};
+
+const getAllStudentsFromDB = async () => {
+  const result = await Student.find();
+  return result;
+};
+
+const getStudentById = async (id: string) => {
+  const result = await Student.findOne({ _id: id });
+  return result;
+};
+
+export const StudentServices = {
+  createStudentIntoDB,
+  getAllStudentsFromDB,
+  getStudentById,
+};
